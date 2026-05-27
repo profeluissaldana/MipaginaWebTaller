1f345d7fafb8067ac5842e7cc3034fe05aac169e
Lucho <profeluissaldana@gmail.com>
2026-05-27 08:29:43 -0300

    agregados contenido de javascript

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +16,7 @@ def visualStudioCode():
 
 @app.route('/HTML_CSS')
 def htmlCss():
-    return render_template('htmlcss.htm')
+    return render_template('htmlcss.html')
 
 @app.route('/Javascript')
 def javaScript():
@@ -25,6 +26,11 @@ def javaScript():
 def gitGitHub():
     return render_template('gitgithub.html')
 
+# NUEVA SECCIÓN
+@app.route('/arquitectura_flask')
+def arquitecturaFlask():
+    return render_template('arquitecturaflask.html')
+
 @app.route('/frameworkflask')
 def frameworkFlask():
     return render_template('frameworkflask.html')
@@ -33,8 +39,10 @@ def frameworkFlask():
 def arduinoUno():
     return render_template('arduino_uno.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-    """ app.run(host="0.0.0.0", port=10000) se debe aplicar para subirlo al servidor RENDER y comentar la línea de arriba """
 
-
+    """
+    Para Render:
+    app.run(host="0.0.0.0", port=10000)
+    """
